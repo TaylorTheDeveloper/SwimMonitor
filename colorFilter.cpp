0eba19051d4677958e4e7428872dc7fc25d6b745
TaylorTheDeveloper <TaylorTheDeveloper@gmail.com>
2014-11-26 21:15:56 -0500

    attempt at diff/currently not working

---
+++ colorFilter.cpp
@@ -0,0 +1,119 @@
+/*
+Taylor Brockhoeft
+First working implementation of color (HSV colorspace) based image segmentation
+
+
+*/
+
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <time.h>
+
+using namespace cv;
+using namespace std;
+
+ int main( int argc, char** argv )
+ {
+    string filename = "MVI_7026.mp4";// Make arv1, probably should
+    //VideoCapture cam(0); //capture the video from web cam
+
+	VideoCapture cam(filename);//Open File
+
+    if ( !cam.isOpened() ) { // if not success, exit program
+         cout << "Cannot open the web cam or file" << endl;
+         return -1;
+    }
+
+    namedWindow("Control",CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+	int lowH = 0;
+	int highH = 105;//Hardcoded 
+
+	int lowS = 95;//Hardcoded
+	int highS = 255;
+
+	int lowV = 0;
+	int highV = 255;
+
+	int sample = 0;//sample rate
+
+	//Create trackbars in "Control" window
+	cvCreateTrackbar("LowH", "Control", &lowH, 179); //Hue (0 - 179)
+	cvCreateTrackbar("HighH", "Control", &highH, 179);
+
+	cvCreateTrackbar("LowS", "Control", &lowS, 255); //Saturation (0 - 255)
+	cvCreateTrackbar("HighS", "Control", &highS, 255);
+
+	cvCreateTrackbar("LowV", "Control", &lowV, 255);//Value (0 - 255)
+	cvCreateTrackbar("HighV", "Control", &highV, 255);
+
+
+	cvCreateTrackbar("FrameSampler", "Control", &sample, 50);
+	int size = 7; //Bloom Size
+	int sizetwo = 3;//Secondary Bloom size
+
+	int frameCount = 0;
+
+	time_t start;
+	time(&start);
+    while (true){
+    	// Count Frames
+    	//cout << frameCount << endl;
+    	// frameCount += 1;
+    	// if(frameCount == 510){
+    	// 	frameCount = 0;
+    	// 	cam.release();
+    	// 	cam.open(filename);//Re-open Stream
+    	// }
+
+        Mat imgOriginal,imgHSV,imgMask,imgRes;
+        //Mat imgResize;
+        //Size size(100,100);
+        //resize(imgOriginal,imgResize, size);
+
+        //bool bSuccess = cam.read(imgOriginal); // read a new frame from video
+        cam >> imgOriginal;
+
+        if(imgOriginal.empty()){
+    		cam.release();
+            break;
+		}
+
+		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+	
+		inRange(imgHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), imgMask); //Threshold the image
+      
+		//morphological opening (removes small objects from the foreground)
+		//Erode and Expand the edges of the mask to eliminate small artifacts
+		erode(imgMask, imgMask, getStructuringElement(MORPH_ELLIPSE, Size(size, size)) );
+		dilate( imgMask, imgMask, getStructuringElement(MORPH_ELLIPSE, Size(size, size)) ); 
+
+		//morphological closing
+		//Expand Mask and shrnk mask to smooth edges
+		dilate( imgMask, imgMask, getStructuringElement(MORPH_ELLIPSE, Size(size, size)) ); 
+		erode(imgMask, imgMask, getStructuringElement(MORPH_ELLIPSE, Size(size, size)) );
+
+		bitwise_not(imgMask,imgMask);//InvertMask
+
+		bitwise_and(imgOriginal,imgOriginal,imgRes,imgMask);
+
+		imshow("Res",imgMask);
+
+//		imshow("Mask", imgMask); //show the thresholded image
+//		imshow("Original", imgOriginal); //show the original image
+
+        if ((char)waitKey(10) == 27){
+    		cam.release();
+            break; 
+       }
+
+    }
+
+    time_t end;
+    time(&end);
+    double seconds = end - start;
+    cout << start << endl << end << endl << seconds << endl;
+
+   return 0;
+}
--- segmentAndTrack.py
+++ segmentAndTrack.py
@@ -1,25 +1,40 @@
 # filterBlue.py
 # Taylor Brockhoeft
 #
-# Ported From Origional C++ code colorFilter.cpp which does the same thing, only in C++
-# The Process is somewhat different, I make use of numpy arrays
+# This program attempts to segment, and then track, swimmers in a pool.
+# Current Status: not working
 #
-# This Program Outlines how it's possible to use the deep
-# blue colors of the pool, agregate them, and then create
-# a mask which allows us to segment the swimmers from the water.
-#
-# It requires a certain degree of calibration, depending on the hues of blue.
-# Implementing a trackbar is trivially easy, so for now, this program will be hardcoded. 
-# In colorfilter.cpp, you can see how the trackbars affect the mask. In colorFilter.py, 
-# the first attempt to re-write this program, you can see good examples on how to implement
-# a trackbar.
+# May need to go about it a different way. This method attempts to get the differences to determine motion
 # 
 #
 
 import cv2
 import numpy as np
 
-#This Function Segements Swimmers from an image, returns the same image after the mask
+#Global Constants
+MHI_DURATION = 0.5
+DEFAULT_THRESHOLD = 32
+MAX_TIME_DELTA = 0.25
+MIN_TIME_DELTA = 0.05
+
+def nothing(*arg, **kw):
+    """Null Function for trackbar"""
+    pass
+
+def draw_motion_comp(vis, (x, y, w, h), angle, color):
+    """Visually isolates where movement is occuring"""
+    cv2.rectangle(vis, (x, y), (x+w, y+h), (0, 255, 0))
+    r = min(w/2, h/2)
+    cx, cy = x+w/2, y+h/2
+    angle = angle*np.pi/180
+    cv2.circle(vis, (cx, cy), r, color, 3)
+    cv2.line(vis, (cx, cy), (int(cx+np.cos(angle)*r), int(cy+np.sin(angle)*r)), color, 3)
+
+def crop(frame):
+    """Crops an Image""" 
+    return frame[100:100, 100:100]
+
+
 def segment(frame):
     """This Function Segments Swimmers from the image, and returns the same frame after the mask"""
     #Blur
@@ -30,10 +45,10 @@ def segment(frame):
     upper_blue = np.array([130,255,255])
 
     # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsvimg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Threshold the HSV image to get only blue colors
-    imgMask = cv2.inRange(hsv, lower_blue, upper_blue)
+    imgMask = cv2.inRange(hsvimg, lower_blue, upper_blue)
 
     #morphological opening (removes small objects from the foreground)
     #Erode and Expand the edges of the mask to eliminate small artifacts
@@ -55,16 +70,51 @@ def segment(frame):
 
 #cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture('MVI_7026.mp4')
+cv2.namedWindow('swimtracker')
+cv2.createTrackbar('threshold', 'swimtracker', DEFAULT_THRESHOLD, 255, nothing)
 
+ret, frame = cap.read()
+#h, w = frame.shape[:2]
+prev_frame = frame.copy()
+prev_frame = segment(prev_frame)
+h, w = frame.shape[:2]
+motion_history = np.zeros((h, w), np.float32)
+hsv = np.zeros((h, w, 3), np.uint8)
+hsv[:,:,1] = 255
 
 while(1):
 
     # Take each frame
-    _, frame = cap.read()
+    ret, frame = cap.read()
 
-    res = segment(frame)
+    frame = segment(frame)
 
-    cv2.imshow('res',res)
+
+    frame_diff = cv2.absdiff(frame, prev_frame)
+    gray_diff = cv2.cvtColor(frame_diff, cv2.COLOR_BGR2GRAY)#
+    thrs = cv2.getTrackbarPos('threshold', 'swimtracker')
+    ret, motion_mask = cv2.threshold(gray_diff, thrs, 1, cv2.THRESH_BINARY)
+    timestamp = cv2.getTickCount() / cv2.getTickFrequency()
+    cv2.updateMotionHistory(motion_mask, motion_history, timestamp, MHI_DURATION)
+    mg_mask, mg_orient = cv2.calcMotionGradient( motion_history, MAX_TIME_DELTA, MIN_TIME_DELTA, apertureSize=5 )
+    seg_mask, seg_bounds = cv2.segmentMotion(motion_history, timestamp, MAX_TIME_DELTA)
+
+    for i, rect in enumerate([(0, 0, w, h)] + list(seg_bounds)):
+        x, y, rw, rh = rect
+        area = rw*rh
+        if area < 64**2:
+            continue
+        silh_roi   = motion_mask   [y:y+rh,x:x+rw]
+        orient_roi = mg_orient     [y:y+rh,x:x+rw]
+        mask_roi   = mg_mask       [y:y+rh,x:x+rw]
+        mhi_roi    = motion_history[y:y+rh,x:x+rw]
+        if cv2.norm(silh_roi, cv2.NORM_L1) < area*0.05:
+            continue
+        angle = cv2.calcGlobalOrientation(orient_roi, mask_roi, mhi_roi, timestamp, MHI_DURATION)
+        color = ((255, 0, 0), (0, 0, 255))[i == 0]
+        draw_motion_comp(frame, rect, angle, color)
+
+    cv2.imshow('result',frame)
 
     if 0xFF & cv2.waitKey(5) == 27:
         break
